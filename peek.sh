5952f84a8d994ae0661849dbe4911f4c673cc020
ivonsc <ivon.sanchez.cardenas@gmail.com>
2024-10-29 19:37:11 +0100

    I changed peek.sh

--- peek.sh
+++ peek.sh
@@ -3,7 +3,18 @@
 
 file=$1
 line=$2
+len=$(wc -l < "$file")
+double=$(($line * 2))
 
-head -n "$line" "$file"
-echo "..."
-tail -n "$line" "$file"
+if [[ -z $line ]]; then
+ line=3
+fi
+
+if [[ $len -le $double ]]; then
+	cat $file
+else
+	head -n 1 "$file"
+	echo "..."
+	tail -n "$line" "$file"
+fi
+
