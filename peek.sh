0746e91560fbb4c5d7a30518431832bf788934d6
ivonsc <ivon.sanchez.cardenas@gmail.com>
2024-10-31 15:48:49 +0100

    I edited peek.sh

--- peek.sh
+++ peek.sh
@@ -18,3 +18,22 @@ else
 	tail -n "$line" "$file"
 fi
 
+#!/bin/bash
+
+
+find . -type f -name "*.txt" | while read -r file; do
+
+    line_count=$(wc -l < "$file")
+
+    if [[ "$line_count" -le 20 ]]; then
+
+        echo "File has $line_count lines, displaying full content:"
+        cat "$file"
+    else
+        echo "WARNING: File has $line_count lines, displaying first and last 10 lines:"
+        head -n 10 "$file"
+        echo "..."
+        tail -n 10 "$file"
+    fi
+
+done
