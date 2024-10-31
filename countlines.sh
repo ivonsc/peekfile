268c82a2e259dec3d8c6a167386eff07eeb04add
ivonsc <ivon.sanchez.cardenas@gmail.com>
2024-10-31 15:50:25 +0100

    I edited countlines.sh

--- countlines.sh
+++ countlines.sh
@@ -1,9 +1,14 @@
-file=$1
+#!/bin/bash
 
-if [[ $(wc -l < "$file") -gt 1 ]]; then
-	echo "$1" has $(wc -l < "$file") lines
-elif [[ $(wc -l < "$file") -eq 1 ]]; then
-	echo "$1" has one line
-else
-	echo "$1" has zero lines
-fi
+for file in "$@"; do
+
+    line_count=$(wc -l < "$file")
+
+    if [[ "$line_count" -gt 1 ]]; then
+        echo "$file has $line_count lines"
+    elif [[ "$line_count" -eq 1 ]]; then
+        echo "$file has one line"
+    else
+        echo "$file has zero lines"
+    fi
+done
